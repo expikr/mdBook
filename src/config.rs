a132734ea0bd281dcdc56c6e3b9aed661d19ff60
expikr <77922942+expikr@users.noreply.github.com>
2023-04-09 17:46:10 +0800

    Additional Theme
    add tests for additional-themes
    
    Update book.js
    
    sanitized default
    
    sanitize the id name in the DOM
    
    catch error if attempting to select or deselect a non-existent theme.
    
    sanitize theme names
    
    Update book.js
    
    Update book.js
    
    Update book.js
    
    Revert "Update book.js"
    
    This reverts commit 0ed9477bf418c48615f7f48fe6209874603401fc.
    
    Update book.js
    
    Update config.rs
    
    Revert "Update hbs_renderer.rs"
    
    This reverts commit 54ad1d530003e8000b725da96cced424e572445c.
    
    Revert "Update config.rs"
    
    This reverts commit 06909a72e5675979f1d686bc2a127b8d77491295.
    
    Revert "Update hbs_renderer.rs"
    
    This reverts commit dc0f9595619dd4ede3f2812d370112ef55ba8f54.
    
    Update hbs_renderer.rs
    
    Update hbs_renderer.rs
    
    Update config.rs
    
    Update book.js
    
    Additional Theme

--- src/config.rs
+++ src/config.rs
@@ -536,6 +536,8 @@ pub struct HtmlConfig {
     pub google_analytics: Option<String>,
     /// Additional CSS stylesheets to include in the rendered page's `<head>`.
     pub additional_css: Vec<PathBuf>,
+    /// Additional theme.
+    pub additional_theme: Vec<PathBuf>,
     /// Additional JS scripts to include at the bottom of the rendered page's
     /// `<body>`.
     pub additional_js: Vec<PathBuf>,
@@ -595,6 +597,7 @@ impl Default for HtmlConfig {
             copy_fonts: true,
             google_analytics: None,
             additional_css: Vec::new(),
+            additional_theme: Vec::new(),
             additional_js: Vec::new(),
             fold: Fold::default(),
             playground: Playground::default(),
@@ -801,6 +804,7 @@ mod tests {
         curly-quotes = true
         google-analytics = "123456"
         additional-css = ["./foo/bar/baz.css"]
+        additional-theme = ["foobar"]
         git-repository-url = "https://foo.com/"
         git-repository-icon = "fa-code-fork"
 
@@ -848,6 +852,7 @@ mod tests {
             curly_quotes: true,
             google_analytics: Some(String::from("123456")),
             additional_css: vec![PathBuf::from("./foo/bar/baz.css")],
+            additional_theme: vec![PathBuf::from("foobar")],
             theme: Some(PathBuf::from("./themedir")),
             default_theme: Some(String::from("rust")),
             playground: playground_should_be,
@@ -1028,6 +1033,7 @@ mod tests {
         curly-quotes = true
         google-analytics = "123456"
         additional-css = ["custom.css", "custom2.css"]
+        additional-theme = ["barfoo"]
         additional-js = ["custom.js"]
         "#;
 
@@ -1053,6 +1059,7 @@ mod tests {
             curly_quotes: true,
             google_analytics: Some(String::from("123456")),
             additional_css: vec![PathBuf::from("custom.css"), PathBuf::from("custom2.css")],
+            additional_theme: vec![PathBuf::from("barfoo")],
             additional_js: vec![PathBuf::from("custom.js")],
             ..Default::default()
         };
